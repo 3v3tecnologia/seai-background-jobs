71f09a568a82a389903cce17a962c7eeaee0184a
Spinnafre <davispenha@gmail.com>
2024-07-09 14:31:11 -0300

    fix: irrigation api base url

--- src/modules/mailer/config/api.js
+++ src/modules/mailer/config/api.js
@@ -9,5 +9,5 @@ export const USERS_SERVICES_API_URL = {
 export const NEWSLETTER_API_BASE_URL = `${process.env.NEWSLETTER_API_ADDR}/api/v1/news`;
 
 export const IRRIGATION_API_BASE_URL =
-  process.env.IRRIGATION_API ||
+  process.env.IRRIGATION_API_ADDR ||
   "http://localhost:8080/api/v2/management/irrigation_crops";

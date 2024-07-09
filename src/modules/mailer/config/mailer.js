861c65cc19665177bb36beb9f98e1930a1187be0
Spinnafre <davispenha@gmail.com>
2024-07-09 17:46:54 -0300

    config: mailer

--- src/modules/mailer/config/mailer.js
+++ src/modules/mailer/config/mailer.js
@@ -1,6 +1,7 @@
 const MAILER_TRANSPORT_CONFIG = {
   port: Number(process.env.MAIL_PORT),
   host: process.env.MAIL_HOST,
+  secure: Number(process.env.MAIL_PORT) === 465 ? true : false,
   auth: {
     user: process.env.MAIL_USERNAME,
     pass: process.env.MAIL_PASSWORD,

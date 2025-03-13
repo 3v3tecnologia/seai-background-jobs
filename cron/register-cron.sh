6e3c380ac4f770e974822ab3328706d5a7e759e9
Spinnafre <davispenha@gmail.com>
2025-03-13 17:39:48 -0300

    fix: irrigation reports DTO

--- cron/register-cron.sh
+++ cron/register-cron.sh
@@ -5,7 +5,7 @@ NODE_PATH=$(which node)
 # Create a new crontab file
 echo "0 0 * * * $NODE_PATH /usr/src/app/src/cron/fetch-equipments.mjs >> /var/log/cron.log 2>&1" >>/etc/cron.d/my-cron-jobs
 echo "0 */8 * * * $NODE_PATH /usr/src/app/src/cron/fetch-measurements.mjs >> /var/log/cron.log 2>&1" >>/etc/cron.d/my-cron-jobs
-echo "0 7 * * * $NODE_PATH /usr/src/app/src/cron/irrigation-reports.mjs >> /var/log/cron.log 2>&1" >>/etc/cron.d/my-cron-jobs
+echo "* * * * * $NODE_PATH /usr/src/app/src/cron/irrigation-reports.mjs >> /var/log/cron.log 2>&1" >>/etc/cron.d/my-cron-jobs
 echo "0 14 * * * $NODE_PATH /usr/src/app/src/cron/newsletter.mjs >> /var/log/cron.log 2>&1" >>/etc/cron.d/my-cron-jobs
 
 chmod +x /usr/src/app/src/cron/fetch-equipments.mjs
